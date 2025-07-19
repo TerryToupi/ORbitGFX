af2f44b7db508155f56a2d53f3f8491ac7e494b0
TerryToupi <terrytoupi@gmail.com>
2025-07-19 18:45:06 +0300

    inverting left and right for imgui

--- src/platform/dawn/frameworks/imgui/imgui_impl_wgpu.cpp
+++ src/platform/dawn/frameworks/imgui/imgui_impl_wgpu.cpp
@@ -304,8 +304,8 @@ static void ImGui_ImplWGPU_SetupRenderState(ImDrawData* draw_data, WGPURenderPas
     // Setup orthographic projection matrix into our constant buffer
     // Our visible imgui space lies from draw_data->DisplayPos (top left) to draw_data->DisplayPos+data_data->DisplaySize (bottom right).
     {
-        float L = draw_data->DisplayPos.x;
-        float R = draw_data->DisplayPos.x + draw_data->DisplaySize.x;
+        float L = draw_data->DisplayPos.x + draw_data->DisplaySize.x;
+        float R = draw_data->DisplayPos.x;
         float T = draw_data->DisplayPos.y;
         float B = draw_data->DisplayPos.y + draw_data->DisplaySize.y;
         float mvp[4][4] =
